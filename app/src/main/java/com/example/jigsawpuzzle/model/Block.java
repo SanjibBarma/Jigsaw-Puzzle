122aaecfad4fc15edd073c9e4edc5ab3d44791de
SanjibBarma <sanjib.barma@v2.ltd>
2024-04-29 14:21:06 +0600

    jigsaw

---
+++ app/src/main/java/com/example/jigsawpuzzle/model/Block.java
@@ -0,0 +1,12 @@
+package com.example.jigsawpuzzle.model;
+
+public class Block {
+    public Block(int position, int vPosition, int hPosition){
+        this.position = position;
+        this.vPosition = vPosition;
+        this.hPosition = hPosition;
+    }
+    public int position;
+    public int vPosition;
+    public int hPosition;
+}
